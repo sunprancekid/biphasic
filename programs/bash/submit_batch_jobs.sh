4e3a5f85cf36d68f91e8e317e03d13ba1ad4e0e1
Matthew Dorsey <matthew.dorsey@mpikg.mpg.de>
2025-08-14 14:21:15 +0200

    Added options, checks for running febio4 simulations locally and via slurm.

--- programs/bash/submit_batch_jobs.sh
+++ programs/bash/submit_batch_jobs.sh
@@ -17,12 +17,18 @@ FILENAME="submit_bash_jobs"
 PARSE_CSV="./programs/bash/util/parse_csv.sh"
 # path to script for slurm submission
 SUB_SLURM="./programs/bash/util/submit_febio_slurm.sh"
+# febio4 executable
+FEBIO4='febio4'
 
 ## OPTION PARAMETERS
 # boolean that determines if the job directory path has been specified
 declare -i BOOL_PATH=0
 # boolean that determines if the job name has been specified
 declare -i BOOL_JOB=0
+# boolean that determines if the jobs should be run locally
+declare -i BOOL_LOCAL=0
+# boolean that determines if the jobs should be run via slurm
+declare -i BOOL_SLURM=0
 
 ## FUNCTIONS
 # display options, exit
@@ -41,12 +47,13 @@ help () {
     echo -e "\n ## SCRIPT PROTOCOL ## \n"
     echo -e " -h\t\t| display options, exit 0"
 #     echo -e " -o\t\t| overwrite files and restart all simulations, even if they have already run."
-#     echo -e " -l\t\t| run job locally ('febio4' must be installed)."
-#     echo -e " -s\t\t| submit job via slurm (via 'sbatch' - see util/submit_febio_slurm.sh)."
+    echo -e " -l\t\t| run job locally ('febio4' must be installed)."
+    echo -e " -s\t\t| submit job via slurm (via 'sbatch' - see ${SUB_SLURM})."
     echo -e "\n ## SCRIPT PARAEMETERS ## \n"
     echo -e " -d  << ARG >>\t| MANDATORY: path to job directory, contains '.feb' file."
     echo -e " -j  << ARG >>\t| MANDATORY: job name, corresponds to a '.csv' file name in \$DIR, which contains job parameters."
 #     echo -e " -f  << ARG >>\t| OPTIONAL: specify a check file: if the file exists within the simulation subdirectory, the script will skip submitting / runnning this simulation."
+    echo -e "/n"
     # exit
     exit $exitcode
 
@@ -107,20 +114,47 @@ check () {
         if [ ! -f $PARM_FILE ]
         then
             # the parameter file does not exist
-            display_error "the parameter file '$PARM_FILE' cannot be found."
+            display_error "the parameter file '$PARM_FILE' cannot be foundfebio4."
             help $NONZEROEXITCODE
         fi
     fi
     # the job name has been specified and the parameter file exists
+
+    # if the local boolean has been specified
+    if [ $BOOL_LOCAL -eq 1 ]
+    then
+        # check if the febio4 command exists
+        if ! command -v $FEBIO4 >/dev/null 2>&1
+        then
+            # if it does not, then jobs cannot be run locally
+            display_error "the command '${febio4}' could not be found, jobs cannot be run locally"
+            help $NONZEROEXITCODE
+        fi
+    fi
+
+    # if the jobs should be run via slurm
+    if [ $BOOL_SLURM -eq 1 ]; then
+        # check if the sbatch command exists
+        if ! command -v sbatch >/dev/null/ 2>&1
+        then
+            # if the sbatch does not exist, jobs cannot be submitted
+            display_error "the command 'sbatch' cound not be found, please log onto computing cluster before exectuing jobs to submit to slurm"
+            help $NONZEROEXITCODE
+        fi
+    fi
 }
 
 ## OPTIONS
 # parse options
-while getopts "hd:j:" opt
+while getopts "hlsd:j:" opt
 do
     case $opt in
         h) # display help options and exit zero
             help 0 ;;
+        l) # run febio locally
+            declare -i BOOL_LOCAL=1 ;;
+        s) # run the jobs via slurm
+            declare -i BOOL_SLURM=1 ;;
         d) # path to job directory
             declare -i BOOL_PATH=1
             JOB_PATH=${OPTARG} ;;
@@ -151,7 +185,7 @@ declare -i N_LINES=$($PARSE_CSV -f $PARM_FILE -l)
 # loop through each line, line 1 is the header ..
 for n in $(seq 2 $N_LINES)
 do
-    ## get simulation paths
+    ## get simulation pathsfebio4
     # the first column is the SUBDIR
     SUBDIR=$($PARSE_CSV -f $PARM_FILE -l $n -c 1)
     # the second column is the SIMID
@@ -161,5 +195,11 @@ do
 
     ## run job ..
     # if local, run on current machine with 'febio4'
-    # if slurm, generate submission script and run sbatch
+    if [ $BOOL_LOCAL -eq 1 ]; then
+        display_error "TODO :: implement running febio4 simulations locally"
+        exit $NONZEROEXITCODE
+    elif [ $BOOL_SLURM -eq 1 ]; then
+        # if slurm, generate submission script and run sbatch
+        display_error "TODO :: implement running febio4 simulations via slurm"
+    fi
 done
