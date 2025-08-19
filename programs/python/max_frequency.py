9f7a3fee1b496f50032faa131f71da4d059c7c4b
Matthew Dorsey <matthew.dorsey@mpikg.mpg.de>
2025-08-19 15:46:27 +0200

    Moved fitting functions to smoothie.

--- programs/python/max_frequency.py
+++ programs/python/max_frequency.py
@@ -11,37 +11,39 @@ import sys, os, math
 import pandas as pd
 import numpy as np
 from scipy.optimize import curve_fit # used for curve fitting
+from scipy.signal import savgol_filter as sg
 import matplotlib.pyplot as plt
 # local
-# none
+# used for filter, fitting, and smoothing data
+from util.smoothie import lin2log, log2lin
+from util.smoothie import poly_smooth as psmooth
 
 ## PARAMETERS
 # nonzero exit code
 nonzero_exitcode=120
 # number of points to use in fit
 n_fit = 100
+# boolean that determines if the fitting should be plotted
+plot = False
+# name of file used to store work information
+hys_out = "hys.out.csv"
+# file that contains maximum frequency predictions
+mf_out = "mf.out.csv"
 
 ## METHODS
-# converts linear scale to log scale
-def lin2log(x, base = 10.):
-    return math.log(x) / math.log(base)
-
-# converts log scale to linear scale
-def log2lin (x, base = 10.):
-    return math.pow(base, x)
-
-# cauchy distribution function
-def cauchy_dist (x, x0, gamma):
-    return (1. / math.pi) * (gamma / (np.pow(x - x0, 2) + math.pow(gamma, 2)))
+# none
 
 
 ## ARGUMENTS
-# first argument: path to file which contains hysteresis information
-hys_file = sys.argv[1]
+# first argument: path to directory which contains hysterseis file
+hys_path = sys.argv[1]
+# second argument: file that contains the hysteresis / cycle information
+hys_file = sys.argv[2]
+
 
 ## SCRIPT
 # open the file, find the columns which correspond to the period and the work data
-df = pd.read_csv(hys_file)
+df = pd.read_csv(hys_path + hys_file)
 head = list(df.columns.values)
 
 # check if the header has columns corresponding to the period
@@ -58,6 +60,7 @@ while True:
         break
 
 ## loop through each cycle, fit data to work data
+mf = []
 for i in range(c):
     # parse the data from the csv
     p = df['period'].to_list()
@@ -67,35 +70,31 @@ for i in range(c):
     for j in range(len(p)):
         p_log.append(lin2log(p[j]))
 
-    # fit the data to cauchy distribution
-    init = [ p_log[w.index(max(w))], 0.0000001] # initial values to start with
-    popt, pcov = curve_fit(cauchy_dist, p_log, w, p0 = init)
-    p_fit = [ ((max(p_log) - min(p_log)) / (n_fit - 1)) * j + min(p_log) for j in range(n_fit)]
-    w_fit = [ cauchy_dist(x, *popt) for x in p_fit ]
-
-    print(popt)
-    print("The maximum period is {} seconds.".format(log2lin(popt[0])))
-
-    # plot to double check
-    fig, ax = plt.subplots()
-    plt.plot(p_fit, w_fit, 'k--',   label = "Cauchy Fit"    )
-    plt.plot(p_log,     w,  'rx',   label = "Hystersis Data")
-    plt.legend(loc = 'upper right')
-    plt.show()
-    exit()
-
-    # print(popt)
-    # exit()
-    #
-    # print("The maximum period is {} seconds".format(popt[0]))
-
-    for j in range(len(p_fit)):
-        print(j, p_fit[j], w_fit[j])
-
-    res = 0.
-    for j in range(len(w)):
-        res += math.pow(w[j] - cauchy_dist(p_log[j], *popt), 2)
-
+    p_filter, w_filter = psmooth (x = p_log, y = w, order = 2, window = len(p_log) - 23, n_fit = 90)
     # find the maximum
-    # write to file
+    mf.append(log2lin(p_filter[w_filter.index(max(w_filter))]))
+    print ("For cycle {} the maximum period is {} seconds.".format(i, mf[-1]))
+
+    if plot:
+        # plot to double check
+        fig, ax = plt.subplots()
+        plt.plot(p_filter, w_filter, 'k--', label = "SG Filter")
+        plt.plot(p_log, w, 'rx', label = "Hystersis Data")
+        plt.legend(loc = 'upper right')
+        plt.show()
+
+# export results to a csv
+# write header and cycle information
+header = ""
+cycle = ""
+for i in range(len(mf)):
+    header += "c{}".format(i)
+    cycle += "{}".format(mf[i])
+    if i != (len(mf) - 1):
+        header += ","
+        cycle += ","
+
+with open(hys_path + mf_out, 'w') as s_io:
+    s_io.writelines("{}\n".format(header))
+    s_io.writelines("{}\n".format(cycle))
 
