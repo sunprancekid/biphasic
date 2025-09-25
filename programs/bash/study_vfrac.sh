e8b13ef182da816cc313228fc7bbf8e473118834
Matthew Dorsey <matthew.dorsey@mpikg.mpg.de>
2025-09-25 16:00:20 +0200

    Initial commit for volume fraction study.

---
+++ programs/bash/study_vfrac.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+## Matthew Dorsey
+## @mad-mpikg
+## MPIKG - matthew.dorsey@mpikg.mpg.de
+## 2025.09.25
+
+## perform max frequency analysis as a function of the volume fraction
+
+
+## PARAMETERS
+# none
+
+
+## FUNCTIONS
+# none
+
+
+## OPTIONS
+# none
+
+
+## ARGUMENTS
+# none
+
+
+## SCRIPT
+# none
