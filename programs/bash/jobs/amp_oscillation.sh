ea225a01350ce3e6bdf24bb8bb74ee09550685a3
Matthew Dorsey <madorse2@ncsu.edu>
2025-11-02 11:00:33 +0100

    Reduced loading depth, increased number of cycles, decreased the sampling frequency during hysteresis.

--- programs/bash/jobs/amp_oscillation.sh
+++ programs/bash/jobs/amp_oscillation.sh
@@ -49,7 +49,7 @@ VOLUME_FRAC="0.2"
 # elastic modulus (MPa)
 ELASTIC_MOD="0.5"
 # loading depth (mm)
-VAL_LOAD_DEPTH="0.1"
+VAL_LOAD_DEPTH="0.05"
 
 # OSCILLATION AMPLITUDE (linscale)
 # minimum amplitude to test (mm)
@@ -57,7 +57,7 @@ MIN_AMPLITUDE="0.02"
 # maximum amplitude depth (mm)
 MAX_AMPLITUDE="0.1"
 # number of amplitudes to test
-N_AMPLITUDE="5"
+N_AMPLITUDE="3"
 
 ## OSCILLATION PERIOD PARAMETERS (logscale)
 # minimum period to test (seconds)
