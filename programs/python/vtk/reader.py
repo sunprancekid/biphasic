9e5e0606d242d36e11e2f4ad413a12f2486ce820
Matthew Dorsey <matthew.dorsey@mpikg.mpg.de>
2025-11-06 17:03:51 +0100

    Initial commit for VTK reader.

---
+++ programs/python/vtk/reader.py
@@ -0,0 +1,28 @@
+
+## Matthew Dorsey
+## @mad-mpikg
+## matthew.dorsey@mpikg.mpg.de
+## Max-Planck-Institute for Colloids and Interfaces
+## 2025.11.06
+
+## FILENAME: programs/python/vtk/reader.py
+## PURPOSE: open vtk files, parse information from fields
+
+## MODULES
+# conda / python native
+# local
+
+## PARAMETERS
+# none
+
+## METHODS
+# none
+
+## CLASSES
+# vtk reader class
+
+## ARGUMENTS
+# none
+
+## SCRIPT
+# none
