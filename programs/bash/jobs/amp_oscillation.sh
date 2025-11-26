da16640db019d7daeacfcaa053a477f5d9b334d0
Matthew Dorsey <madorse2@ncsu.edu>
2025-11-26 09:13:34 -0600

    Update oscillation amplitude testing parameters.:

--- programs/bash/jobs/amp_oscillation.sh
+++ programs/bash/jobs/amp_oscillation.sh
@@ -57,7 +57,7 @@ MIN_AMPLITUDE="0.02"
 # maximum amplitude depth (mm)
 MAX_AMPLITUDE="0.1"
 # number of amplitudes to test
-N_AMPLITUDE="3"
+N_AMPLITUDE="5"
 
 ## OSCILLATION PERIOD PARAMETERS (logscale)
 # minimum period to test (seconds)
