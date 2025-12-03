93b2dd883e01f057ec78e0acfb7e91d578ce295e
Matthew Dorsey <matthew.dorsey@mpikg.mpg.de>
2025-12-03 18:11:06 +0100

    Updated amplitude parameters.

--- programs/bash/jobs/amp_oscillation.sh
+++ programs/bash/jobs/amp_oscillation.sh
@@ -49,15 +49,15 @@ VOLUME_FRAC="0.2"
 # elastic modulus (MPa)
 ELASTIC_MOD="0.5"
 # loading depth (mm)
-VAL_LOAD_DEPTH="0.05"
+VAL_LOAD_DEPTH="0.1"
 
 # OSCILLATION AMPLITUDE (linscale)
 # minimum amplitude to test (mm)
-MIN_AMPLITUDE="0.02"
+MIN_AMPLITUDE="0.01"
 # maximum amplitude depth (mm)
 MAX_AMPLITUDE="0.1"
 # number of amplitudes to test
-N_AMPLITUDE="5"
+N_AMPLITUDE="10"
 
 ## OSCILLATION PERIOD PARAMETERS (logscale)
 # minimum period to test (seconds)
