528eaf9b62fa553651a0b5156db869ae78fa798f
Matthew Dorsey <matthew.dorsey@mpikg.mpg.de>
2026-01-29 17:49:51 +0100

    Initial commit for relaxation time parameter file.

---
+++ programs/bash/parameter/loading/relaxation-time.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+## Matthew A. Dorsey
+## @mad-mpikg
+## matthew.dorsey@mpikg.mpg.de
+## Max-Planck-Institute for Colloids and Interfacial Sciences
+## 2026.01.29
+
+## FILENAME: programs/bash/parameters/loading/relaxation-time.sh
+## PURPOSE: parameterize relaxation time of loading
+
+## MODULES
+# none
+
+
+## PARAMETERS
+# none
+
+
+## FUNCTIONS
+# none
+
+
+## OPTIONS
+# none
+
+
+## ARGUMENTS
+# none
+
+
+## SCRIPT
+# none
