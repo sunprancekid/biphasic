90949a1fd9dac5a1b549a7789b235bd985f8d01f
Matthew Dorsey <matthew.dorsey@mpikg.mpg.de>
2026-02-06 16:03:07 +0100

    Initial commit.

---
+++ programs/python/febio/simulation.py
@@ -0,0 +1,116 @@
+
+## Matthew A. Dorsey
+## @mad-mpikg
+## matthew.dorsey@mpikg.mpg.de
+## Max Planck Institute for Colloids and Interfacial Sciences
+## 06.02.2026
+
+## FILENAME: programs/python/febio/simulation.py
+## PURPOSE: contains classes and methods for handling febio simulation
+
+## MODULES
+# native / conda
+import sys, os
+import pandas as pd
+
+## PARAMETERS
+# none
+
+## METHODS
+# none
+
+## CLASSES
+# simulation class
+class Simulation (object):
+
+    """ handles febio simulation data.
+
+    Attributes:
+    -----------
+    None
+
+    Methods
+    -------
+    None
+    """
+
+    def __init__ (self, jd, jn, si):
+        """ initialize simulation path using job directory.
+
+        Parameter:
+        ----------
+        jd : str
+            path to job directory
+        jn : str
+            job name
+        si : int
+            integer corresponding to the simulation number in the job
+
+        Returns:
+        --------
+        None
+        """
+        # get the row corresponding to the simulation from the parameter file
+        df_parm = pd.read_csv("{0}/{1}/{1}.parm.csv".format(jd, jn))
+        self.parm = df_parm.iloc[si - 1]
+        # files
+        self.file_feb = None
+        self.file_xplt = None
+        self.file_log = None
+        # open parameter file, get parameters
+        # open feb file, extract feb information (if needed)
+        # check febio.out files, xplt files ...
+
+    def has_key (self, k):
+        """ check if key exists as job parameter.
+
+        Paramters:
+        ----------
+        k : str
+            header in parameter file column
+
+        Returns:
+        --------
+        bool
+            bool determining if key exist in job parameter file
+        """
+        return (k in self.parm)
+
+    def get_key_value (self, k):
+        """ get key value for simulation from job parameters.
+
+        Parameters:
+        -----------
+        k : str
+            header in parameter file
+
+        Returns:
+        --------
+        str
+            value stored in parameter file
+        """
+        return self.parm[k]
+
+    def has_logfile (self):
+        pass
+
+    def parse_logfile (self):
+        pass
+
+    def parse_prestress_work (self):
+        pass
+
+    def parse_hystersis_work (self):
+        pass
+
+    def show_hystersis (self):
+        pass
+
+    def feb_has_path (self):
+        pass
+
+## ARGUMENTS
+# none
+
+## SCRIPT
+# none
