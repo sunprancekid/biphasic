301834c64baaaacbf37961043b1a734994b8feb1
Matthew Dorsey <matthew.dorsey@mpikg.mpg.de>
2026-02-12 16:39:29 +0100

    Initial commit for sweep object.

---
+++ programs/python/febio/sweep.py
@@ -0,0 +1,187 @@
+
+## Matthew A. Dorsey
+## @mad-mpikg
+## matthew.dorsey@mpikg.mpg.de
+## Max-Planck-Institut for Colloids and Interfacial Sciences
+## 2026.02.12
+
+## FILENAME: programs/python/febio/sweep.py
+## PURPOSE: handles simulation sets with variable oscilation frequency
+
+## MODULES
+# native / conda
+import sys, os, math
+import pandas as pd
+# local
+from simulation import Simulation
+
+
+## PARAMETERS
+# none
+
+
+## METHODS
+# none
+
+
+## CLASSES
+class Sweep (object):
+    """ handles simulation sets with variable oscillation frequency.
+
+    Attributes:
+    -----------
+    list_sim : List[Simulation]
+      list of simulation objects
+    parm_sim : dict
+        simulation parameters stored in dictionary
+
+    Methods:
+    --------
+    None
+
+    """
+    __init__ (self):
+        # initialize the list
+        self.list_sim = []
+        self.parm_sim = []
+
+    ## SIMULATIONS
+
+    def get_sim_num (self):
+        """ returns number of simulations stored within object.
+
+        Paramters:
+        ----------
+        None
+
+        Returns:
+        --------
+        None
+        """
+        pass
+
+    def add_sim(self, jd, jn, si):
+        """ append simulation to list.
+
+        Attempts to add simulation to list_sim. Checks simulation parameters
+        against parameters for simulations already in list. If all other
+        simulation parameters other than the osciliation time are the same,
+        then the simulation is included with the others. Otherwise, the
+        simulation cannot be added. The boolean returned by the method
+        determines if the appending operation was successful.
+
+        Parameters:
+        -----------
+        jd : str
+            path to job directory
+        jn : str
+            job name
+        si : int
+            integer corresponding to simulation in job
+
+        Return:
+        -------
+        bool
+            True if addition was successful, else False.
+        """
+        pass
+
+    ## ANALYSIS - HYSTERESIS WORK ##
+
+    def get_hysteresis_work (self):
+        """ returns the hysteresis work for all simulations.
+
+        Parameters:
+        -----------
+        None
+
+        Returns:
+        --------
+        None
+
+        """
+        pass
+
+    def show_work (self):
+        """ plot hysteresis work.
+
+        Parameters:
+        -----------
+        None
+
+        Returns:
+        --------
+        None
+
+        """
+        pass
+
+
+    ## ANALYSIS - PHASE SHIFT ##
+
+    def get_phase_shift (self):
+        """ returns the phase shift (deg) in dynamic response for all simulations.
+
+        Parameters:
+        -----------
+        None
+
+        Returns:
+        --------
+        None
+
+        """
+        pass
+
+    def show_phase_shift (self):
+        """ displays the phase shift (deg) for all simulations against frequency.
+
+        Parameters:
+        -----------
+        None
+
+        Returns:
+        --------
+        None
+
+        """
+        pass
+
+
+    ## ANALYSIS - DYNAMIC MODULUS ##
+
+    def get_dynamic_modulus (self):
+        """ returns the dynamic modulus (Pa) for all simulations and all cycles.
+
+        Parameters:
+        -----------
+        None
+
+        Returns:
+        --------
+        None
+
+        """
+        pass
+
+    def show_dynamic_modulus (self):
+        """ displays the dynamic modulus for all simulations against frequency.
+
+        Parameters:
+        -----------
+        None
+
+        Returns:
+        --------
+        None
+
+        """
+        pass
+
+
+## ARGUMENTS
+# none
+
+
+## SCRIPT
+# none
