69cf80d007e4bb76669113be7438b2a2435f34c8
Matthew Dorsey <matthew.dorsey@mpikg.mpg.de>
2026-02-20 08:50:03 +0100

    Added viscoelastic modulation.

---
+++ projects/viscoelastic/viscoelastic_modulation.py
@@ -0,0 +1,36 @@
+
+## Matthew A. Dorsey
+## matthew.dorsey@mpikg.mpg.de
+## @mad-mpikg
+## Max Planck Institute for Colloids and Interfaces
+## 2026.02.19
+
+## FILENAME: projects/viscoelastic/viscoelastic_modulation.py
+## PURPOSE: sysmteatically vary the viscoelastic parameters
+
+## MODULES
+# native / conda
+import sys, os
+import pandas as pd
+# local
+# none
+
+
+## PARAMETERS
+# none
+
+
+## METHODS
+# none
+
+
+## CLASSES
+# none
+
+
+## ARGUMENTS
+# none
+
+
+## SCRIPT
+# none
