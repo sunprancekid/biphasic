1027b2f0a2f908aa46ca901641cd674610a82222
Matthew Dorsey <madorse2@ncsu.edu>
2026-05-06 19:25:25 +0200

    Added debugging statement.

--- programs/bash/simulation/run.sh
+++ programs/bash/simulation/run.sh
@@ -275,7 +275,7 @@ submit () {
         fi
         # submit the script from the local directory
         # declare -i slurmid="$($SUB_SLURM -d ${simdirstack} -f ${simdirstack}${simid}.feb -j ${JOB}-${simint} -r $SLURM_FLAGS )"
-        echo "$($SUB_SLURM -d ${simdirstack} -f ${simdirstack}${simid}.feb -j ${JOB}-${simint} -r $SLURM_FLAGS )"
+        echo "$SUB_SLURM -d ${simdirstack} -f ${simdirstack}${simid}.feb -j ${JOB}-${simint} -r $SLURM_FLAGS"
         echo "${JOB}${simint} (slurm id: ${simid}): $slurmid"
         return
     fi
