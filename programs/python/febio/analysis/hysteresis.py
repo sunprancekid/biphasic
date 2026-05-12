1c4a4a3a42b9c9bcc2ec5cc6c301b523351d7248
Matthew Dorsey <matthew.dorsey@mpikg.mpg.de>
2026-05-12 10:16:08 +0200

    Updated hysteresis display.

--- programs/python/febio/analysis/hysteresis.py
+++ programs/python/febio/analysis/hysteresis.py
@@ -232,7 +232,8 @@ def calculate_complex_mod (period, time, pos, force):
     fig = Figure()
     fig.load_data(df, xcol = 'x', ycol = 'y', icol = 'l')
     fig.set_subtitle_label("$\\delta = {0:.4f}, Y' = {1:.2f}$".format(delta[sub_int], dymod[sub_int]))
-    gen_plot(fig, show = True, save = False)
+    fig.save_data()
+    gen_plot(fig, show = True, save = True)
 
     return delta, dymod
 
