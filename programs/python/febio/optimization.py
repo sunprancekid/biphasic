40b7630eb498efdf8f6f96878f5a8c6f774569be
Matthew Dorsey <matthew.dorsey@mpikg.mpg.de>
2026-07-13 16:23:58 +0200

    Added to optimization routine .

--- programs/python/febio/optimization.py
+++ programs/python/febio/optimization.py
@@ -278,13 +278,18 @@ class Optimization (object):
             return
 
         # check that the results exist within the simulation directory
-        opt_csv_file = "{0}{1}/{2}/febio4.opt.csv".format(self.jd, self.jn, self.df_parm.iloc['n', n])
-        print(opt_csv_file)
-        if not os.path.exists("{0}{1}"):
+        opt_csv_file = "{0}{1}/{2}febio4.opt.csv".format(self.jd, self.jn, self.df_parm.iloc[n]["path"])
+        if not os.path.exists(opt_csv_file):
+            # if the optimization file does not exist within the simulation directory,
+            # attempt to parse if from the job file (if it exists)
+            opt_out_file = ""
+            if not os.path.exists(opt_out_file):
+                #
+            else:
+                # parse the optimization file
 
-
-        # results the results
-        pass
+        # open the opimization file as a data frame, return
+        return pd.read_csv(opt_csv_file)
 
     ## CONFIG ##
 
