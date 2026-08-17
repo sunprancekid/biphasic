02c645fc9fbd75b512bbbfa0a8d9d842e9c4232b
Matthew Dorsey <matthew.dorsey@mpikg.mpg.de>
2026-08-17 11:58:52 +0200

    added file structure and initial method.

---
+++ programs/python/febio/slurm/submit.py
@@ -0,0 +1,37 @@
+
+## Matthew A Dorsey
+## @mad-mpikg
+## Max-Planck-Institute for Colloids and Interfaces
+## 2026.08.17
+## used to create scripts used during slurm submission
+
+## MODULES
+# native
+import sys, os
+
+## PARAMETERS
+# none
+
+## METHODS
+# generates slurm script
+def gen_slurm_script (path = None):
+	""" generates slurm submission script.
+
+	Arguments:
+	----------
+	None
+
+	Returns:
+	--------
+	None
+	"""
+	pass
+
+## CLASSES
+# none
+
+## ARGUMENTS
+# none
+
+## SCRIPT
+# none
