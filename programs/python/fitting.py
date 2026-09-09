4c24b45f746b6b16a7a2914f1b85f54a1e646836
Matthew Dorsey <dorsey@scnb-28z6yk4.mpikg.local>
2026-09-09 07:20:02 +0200

    bug fix.

--- programs/python/fitting.py
+++ programs/python/fitting.py
@@ -418,6 +418,7 @@ def update_optimization (jd = None, jn = None, i = None, overwrite = True, z_int
     j_op = Opt ("{0}{1}/".format(jd, jn), 'opt')
     j_ve = Job ("{0}{1}/".format(jd, jn), 've')
     m_op = ModelFile ("{0}{1}/opt/opt.feb".format(jd, jn))
+    m_ve = ModelFile ("{0}{1}/ve/ve.feb".format(jd, jn))
 
     ## check arguments
     # check job paths
